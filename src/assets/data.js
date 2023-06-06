3a8ab10412fef6465acbd3e7a4c27f9498167bea
houcem <houcem.ellouze.28325972@gmail.com>
2023-06-06 23:20:42 +0100

    thi commit

--- src/assets/data.js
+++ src/assets/data.js
@@ -29,21 +29,21 @@ export const links = [
     id: 2,
     name: 'About',
     icon: <FaUser className='nav__icon' />,
-    path: '/about',
+    path: '/portfolio-react/about',
   },
 
   {
     id: 3,
     name: 'Projects',
     icon: <FaFolderOpen className='nav__icon' />,
-    path: '/portfolio',
+    path: '/portfolio-react/portfolio',
   },
 
   {
     id: 4,
     name: 'Contact',
     icon: <FaEnvelopeOpen className='nav__icon' />,
-    path: '/contact',
+    path: '/portfolio-react/contact',
   },
 ];
 
